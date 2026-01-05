41a66463565914b62364206af3e7c11064139d6f
AshishShrivastva <shrivashish2005@gmail.com>
2026-01-05 10:17:02 +0530

    set up utility functions

---
+++ frontend/src/store/authSlice.ts
@@ -0,0 +1,29 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+
+interface AuthState {
+    status: boolean;
+    userData: null | object;
+}
+
+const initialState: AuthState = {
+    status: false,
+    userData: null,
+};
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        login: (state, action: PayloadAction<object>) => {
+            state.status = true;
+            state.userData = action.payload;
+        },
+        logout: (state) => {
+            state.status = false;
+            state.userData = null;
+        },
+    },
+});
+
+export const { login, logout } = authSlice.actions;
+export default authSlice.reducer;
